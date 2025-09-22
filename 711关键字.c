861235d859ee53bd34c19b1c29230bbe6c8fb114
guisan <hybsfcbztk@outlook.com>
2025-09-22 15:51:38 +0800

    Update 711关键字.c

--- 711关键字.c
+++ 711关键字.c
@@ -1,6 +1,6 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
-//typedef ������������ֻ���������
+//typedef 类型重命名，只能针对类型
 
 typedef unsigned int unit;
 int main()
@@ -17,6 +17,12 @@ typedef struct Node
 	int data;
 	struct Node* nextl
 }Node;
+//写法二：
+//typedef struct
+//{
+//	int data;
+//	struct Node* nextl
+//}Node;
 int main()
 {
 	struct Node n;
@@ -27,17 +33,17 @@ int main()
 
 
 
-//static��
-//c�����У�static���������α����ͺ���ģ�
-//1.���ξֲ�����-��Ϊ��̬�ֲ�����
-//2.����ȫ�ֱ���-��Ϊ��̬ȫ�ֱ���
-//3.���κ���-��Ϊ��̬����
+//static：
+//c语言中，static是用来修饰变量和汉书的：
+//1.修饰局部变量-称为静态局部变量
+//2.修饰全局变量-称为静态全局变量
+//3.修饰函数-称为静态函数
 
 
-//1.���ξֲ�����
-void test()//void��ʾ��������Ҫ�κη���
+//1.修饰局部变量
+void test()//void表示函数不需要任何返回
 {
-	static int a = 1;//�������static����
+	static int a = 1;//加入进行static辨析
 	a++;
 	printf("%d\n", a);
 }
@@ -51,23 +57,24 @@ int main()
 	}
 	return 0;
 }
-//ע��static���ξֲ�������ʱ�򣬾ֲ��������������򣬲����١�
-//�����ϣ�static���ξֲ�������ʱ�򣬸ı��˱����Ĵ洢λ�á�
-//Ӱ���˱������������ڣ��������ڱ䳤���ͳ������������һ�¡�            ������������Դ�ļ��ĺ�����������Ҫ��ǰ����������extern
+//注：static修饰局部变量的时候，局部变量出了作用域，不销毁。
+//本质上，static修饰局部变量的时候，改变了变量的存储位置。
+//影响了变量的生命周期，生命周期变长，和程序的生命周期一致。            附：调用其他源文件的函数，变量需要提前声明，借用extern
 
-//2.����ȫ�ֱ���
-//ȫ�ֱ����Ǿ����ⲿ�������Եģ���static����ȫ�ֱ�����ʱ�����ȫ�ֱ������ⲿ�������Ծͱ�����ڴ��������ԡ�
-//����Դ�ļ�(.c)�Ͳ�����ʹ�õ����ȫ�ֱ����ˡ�
+//2.修饰全局变量
+//全局变量是具有外部链接属性的，但static修饰全局变量的时候，这个全局变量的外部链接属性就变成了内存链接属性。
+//其他源文件(.c)就不能再使用到这个全局变量了。
 
-//3.���κ���
-//һ�����������Ǿ����ⲿ�������Եģ����Ǳ�static���ε�ʱ���ⲿ�������Ծͱ�����ڲ��������ԣ�����Դ�ļ�(.c)���޷�ʹ���ˡ�
+//3.修饰函数
+//一个函数本来是具有外部链接属性的，但是被static修饰的时候，外部链接属性就变成了内部连接属性，其他源文件(.c)就无法使用了。
 
-//�Ĵ������� register
+//寄存器变量 register
 int main()
 {
-	register int num = 3;//���飬3����ڼĴ��У�ע���ǽ��飬����һ�������ڼĴ�����
+	register int num = 3;//建议，3存放在寄存中，注意是建议，并非一定储存在寄存器中
 	return 0;
 }
 
 
-// Ӳ��--�ڴ�--���ٻ���--�Ĵ������������������ٶȱ�죬�ռ��С����۱��
+
+// 硬盘--内存--高速缓存--寄存器（从左至右运行速度变快，空间变小，造价变高
